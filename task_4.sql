16f2aae4ad852bb698ed0c26562bd400db3c0a18
benbraikilyas <168174990+benbraikilyas@users.noreply.github.com>
2025-02-03 18:54:58 +0100

    task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
--- Use the specified database
+-- Use the correct database
 USE alx_book_store;
 
--- Retrieve the full description of the 'books' table
+-- Retrieve the full description of the 'Books' table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA 
 FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
